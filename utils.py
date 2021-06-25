05bb4cc95e566d3ad3f9e3b96f983d33ea9681ea
RFilyk <filyk31@gmail.com>
2021-06-25 09:05:15 +0300

    docker

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 
-cfgstring = 'dbname=postgres user=postgres password=Hrvlgu_#!08'
+cfgstring = 'host=potgres dbname=postgres user=postgres password=Hrvlgu_#!08'
 SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
 
 def mapKeys(key, value):
